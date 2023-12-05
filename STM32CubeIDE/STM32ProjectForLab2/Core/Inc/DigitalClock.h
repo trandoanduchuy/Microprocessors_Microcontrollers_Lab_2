834076d442aaddc4dbc4044dcb5406a1c6f0deb3
huy.trandoanduc <huyducdoantran@gmail.com>
2023-12-06 00:26:53 +0700

    Init exercise 5

---
+++ STM32CubeIDE/STM32ProjectForLab2/Core/Inc/DigitalClock.h
@@ -0,0 +1,17 @@
+/*
+ * DigitalClock.h
+ *
+ *  Created on: Dec 4, 2023
+ *      Author: Huy
+ */
+
+#ifndef INC_DIGITALCLOCK_H_
+#define INC_DIGITALCLOCK_H_
+
+#include "global.h"
+
+extern int hour;
+extern int minute;
+extern int second;
+
+#endif /* INC_DIGITALCLOCK_H_ */
